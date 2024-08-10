4efc19aa8f54ce2792046c297a358fd95e6608f2
arlohb <arlo@urwick.co.uk>
2024-08-10 14:52:34 +0100

    Completed download_linked

--- lua/lecture-notes/downloader.lua
+++ lua/lecture-notes/downloader.lua
@@ -1,13 +1,14 @@
 local M = {}
 
-local config = require("lecture-notes.config").config
 local files = require("lecture-notes.files")
 
 ---Calls callback with true on success and false on failure
 ---@param url string
 ---@param folder string
----@param callback function
+---@param callback function (status: boolean, outfile: string)
 function M.try_download(url, folder, callback)
+    local config = require("lecture-notes.config").config
+
     vim.system(
         {
             vim.fn.expand(config.moodle_simple_dl_exe),
@@ -17,18 +18,11 @@ function M.try_download(url, folder, callback)
             url,
             "--outfolder",
             folder,
-            "--preview",
         },
         {},
         function(output)
-            vim.notify(output.stdout)
-            vim.notify(output.stderr)
-            -- If failed, prompt for new session token
-            if output.code ~= 0 then
-                callback(false)
-            else
-                callback(true)
-            end
+            local outfile = output.stdout
+            callback(output.code == 0, outfile)
         end
     )
 end
@@ -36,11 +30,13 @@ end
 ---Will try again after prompting for a session token if the first try failed
 ---@param url string
 ---@param folder string
----@param callback function
+---@param callback function (outfile: string)
 function M.download_url(url, folder, callback)
-    M.try_download(url, folder, function(success)
+    local config = require("lecture-notes.config").config
+
+    M.try_download(url, folder, function(success, outfile)
         if success then
-            callback()
+            callback(outfile)
             return
         end
 
@@ -48,10 +44,13 @@ function M.download_url(url, folder, callback)
             vim.ui.input({ prompt = "Session Token" }, function(session)
                 config.moodle_simple_dl_session = session
 
-                M.try_download(url, folder, function(success2)
+                M.try_download(url, folder, function(success2, outfile2)
                     if not(success2) then
                         vim.notify("Failed to download file", vim.log.levels.ERROR)
+                        return
                     end
+
+                    callback(outfile2)
                 end)
             end)
         end)
@@ -63,12 +62,27 @@ function M.download()
         require("lecture-notes.downloader").download_url(
             url,
             files.current_folder(),
-            function() end
+            function(_) end
         )
     end)
 end
 
 function M.download_linked()
+    local line = vim.api.nvim_get_current_line()
+    local url = line:match("%b()"):sub(2, -2)
+
+    M.download_url(url, files.current_folder() .. "/Files", vim.schedule_wrap(function(outfile)
+        -- Remove ./ at start
+        outfile = outfile:gsub("^%./", "")
+        -- Remove newline
+        outfile = outfile:gsub("\n", "")
+        -- Make absolute path
+        if outfile:sub(1, 1) ~= "/" then
+            outfile = vim.fn.getcwd() .. "/" .. outfile
+        end
+        line = line:gsub("%b()", "(file://" .. outfile .. ")")
+        vim.api.nvim_set_current_line(line)
+    end))
 end
 
 return M
