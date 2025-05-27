eceecfcf839e767ed433ff7717cb34255c8b7857
arlohb <arlo@urwick.co.uk>
2025-05-27 11:51:20 +0100

    Added package

--- flake.nix
+++ flake.nix
@@ -8,12 +8,19 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages."${system}";
+
+        lecture-notes-nvim = pkgs.vimUtils.buildVimPlugin {
+          name = "lecture-notes.nvim";
+          src = ./.;
+        };
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             lua-language-server
           ];
         };
+
+        packages.default = lecture-notes-nvim;
       }
     );
 }
